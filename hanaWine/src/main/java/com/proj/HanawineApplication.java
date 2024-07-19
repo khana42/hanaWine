ab57b41ba630ad03102cf48d38f7badb73b533b3
hana <khn5963@naver.com>
2024-07-19 11:20:09 +0900

    commit1

---
+++ hanaWine/src/main/java/com/proj/HanawineApplication.java
@@ -0,0 +1,13 @@
+package com.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanawineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanawineApplication.class, args);
+	}
+
+}
