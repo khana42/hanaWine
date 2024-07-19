8a4bf49aaeb0658e9e7e3b05fa60e5daf0777084
useok <us000000@naver.com>
2024-07-19 13:27:34 +0900

    커밋

---
+++ hanaWine/src/main/java/com/proj/MainCotroller.java
@@ -0,0 +1,15 @@
+package com.proj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainCotroller {
+
+	@RequestMapping("/")
+	public String subpage1() {
+		return "subpage1";
+	}
+	
+	
+}
